6da01b0e2ee9b049b0a1f2b08585490e119aed4f
Björn <bjorn.sundling@gmail.com>
2025-12-26 22:37:45 +0100

    Duplicated code is bad..

--- Source/Public/Get-PSNoteIndexFromNoteName.ps1
+++ Source/Public/Get-PSNoteIndexFromNoteName.ps1
@@ -6,9 +6,5 @@ Function Get-PSNoteIndexFromNoteName {
         [string]$NoteName
     )
 
-    $null = $NoteName -match '^(?<BaseNote>[a-gA-G][#b]?)(?<Octave>(-?[1-2]|[0-8])$)'
-    
-    $noteNames  = 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
-
-    ($noteNames.IndexOf($Matches['BaseNote'].ToUpper())) + (([int]$Matches['Octave'] + 2) * 12)
+    [chord]::new($NoteName)
 }
